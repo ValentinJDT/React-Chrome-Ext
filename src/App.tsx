73aba737a6b12a680edd8f74e5aae76388e52f80
Valentin JEANDOT <valentin.jeandot@cirly.com>
2024-04-11 15:58:21 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Dispatch, useEffect, useState } from "react";
+import "./App.css";
+
+interface Size {
+  x: number | string;
+  y: number | string;
+}
+
+interface RouteProps {
+  navigate: Dispatch<React.SetStateAction<string>>;
+  location: string;
+  extWindow: {
+    setSize: (x: number | string, y: number | string) => void;
+    setX: Dispatch<React.SetStateAction<number | string>>;
+    setY: Dispatch<React.SetStateAction<number | string>>;
+    size: Size;
+  };
+}
+
+/** Define your routes and component here. */
+const router: {
+  [path: string]: (props: RouteProps) => JSX.Element;
+} = {
+  "/": Home,
+  "/settings": Settings,
+};
 
 function App() {
+  // Props for routes
+  const [innerPath, setInnerPath] = useState("/");
+
+  const [x, setX] = useState<number | string>("10em");
+  const [y, setY] = useState<number | string>("10em");
+
+  const setSize = (x: number | string, y: number | string) => {
+    setX(x);
+    setY(y);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      width: x,
+      height: y,
+    }}>
+      {router[innerPath] ? (
+        router[innerPath]({
+          location: innerPath,
+          navigate: setInnerPath,
+          extWindow: {
+            setSize: setSize,
+            setX: setX,
+            setY: setY,
+            size: {
+              x: x,
+              y: y,
+            },
+          },
+        })
+      ) : (
+        <h1>404</h1>
+      )}
     </div>
+  );
+}
+
+/** Page home */
+function Home({ location, navigate, extWindow }: RouteProps) {
+  // Define the size of the window
+  useEffect(() => extWindow.setSize("10em", "20em"), [location]);
+
+  return (
+    <>
+      <h1>Test ma page</h1>
+      <button onClick={() => navigate("/settings")}>Go to settings</button>
+      <button onClick={() => console.log("Log in the chrome extension")}>
+        Send log
+      </button>
+    </>
+  );
+}
+
+/** Page setting */
+function Settings({ location, navigate, extWindow }: RouteProps) {
+  
+  // Define the size of the window
+  useEffect(() => extWindow.setSize("10em", "10em"), [location]);
+
+  return (
+    <>
+      <h1>Settings</h1>
+      <button onClick={() => navigate("/")}>Go to home</button>
+    </>
   );
 }
 
