3b184b7d5f7fd9e583cff081899034f3a3316418
ValentinJDT <vjeandot@yahoo.com>
2024-04-14 02:05:02 +0200

    add goBack example

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,17 @@
 import { FC, useEffect } from "react";
 import { RouteComponentProps } from "../component/router/Router";
 
-const Home: FC<RouteComponentProps> = ({ location, navigate, container }) => {
-  useEffect(() => container.setSize("20em", "25em"), [location]);
+const Home: FC<RouteComponentProps> = ({
+  navigation: { location, navigate, goBack },
+  container: { setSize },
+}) => {
+  useEffect(() => setSize("20em", "25em"), [location]);
 
   return (
     <>
       <div>Home</div>
       <button onClick={() => navigate("/settings")}>Go to settings</button>
+      <button onClick={() => goBack()}>Go to previous page</button>
     </>
   );
 };
--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -1,13 +1,17 @@
 import { FC, useEffect } from "react";
 import { RouteComponentProps } from "../component/router/Router";
 
-const Settings: FC<RouteComponentProps> = ({ navigate, container }) => {
-  useEffect(() => container.setSize("10em", "10em"), [location]);
+const Settings: FC<RouteComponentProps> = ({
+  navigation: { navigate, goBack },
+  container: { setSize },
+}) => {
+  useEffect(() => setSize("10em", "10em"), [location]);
 
   return (
     <>
       <div>My settings</div>
-      <button onClick={() => navigate("/")}>Go back to home</button>
+      <button onClick={() => navigate("/")}>Go to home</button>
+      <button onClick={() => goBack()}>Go to previous page</button>
     </>
   );
 };
