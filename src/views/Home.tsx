7d72faab7305d3dc5e785fe974c65d33818d64d2
ValentinJDT <vjeandot@yahoo.com>
2024-04-14 02:21:06 +0200

    add chrome api type and usage example

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { RouteComponentProps } from "../component/router/Router";
 
 const Home: FC<RouteComponentProps> = ({
@@ -7,11 +7,25 @@ const Home: FC<RouteComponentProps> = ({
 }) => {
   useEffect(() => setSize("20em", "25em"), [location]);
 
+  const [currentPath, setCurrentPath] = useState("")
+
+  /** Get current path with chrome api. */
+  const getCurrentTabUrl = () => {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    }, ([tab]) => {
+      console.log(tab.url)
+    });
+  }
+
   return (
     <>
       <div>Home</div>
       <button onClick={() => navigate("/settings")}>Go to settings</button>
       <button onClick={() => goBack()}>Go to previous page</button>
+
+      <button onClick={() => getCurrentTabUrl()}>Show current path : {currentPath}</button>
     </>
   );
 };
